75ba59b47f516733d55848559f51824ecfb38d89
dazuiba <come2u@gamil.com>
2011-09-16 17:23:35 +0800

    tmpl commit

---
+++ Classes/PlayerController.h
@@ -0,0 +1,16 @@
+//
+//  PlayerController.h
+//  iOctocat
+//
+//  Created by sam on 11-9-16.
+//  Copyright 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface PlayerController : NSObject {
+    
+}
+
+@end
---
+++ Classes/PlayerController.m
@@ -0,0 +1,14 @@
+//
+//  PlayerController.m
+//  iOctocat
+//
+//  Created by sam on 11-9-16.
+//  Copyright 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import "PlayerController.h"
+
+
+@implementation PlayerController
+
+@end
